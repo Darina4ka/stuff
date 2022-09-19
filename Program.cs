97e48f39076d3b85bd6821351d354c485224c087
Daria <dashamelnik293@gmail.com>
2022-09-19 13:18:15 +0300

    done first task

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int Exponentiation(int A, int B)
+{
+  int res = 1;
+  for(int i=1; i <= B; i++)
+  {
+    res = res * A;
+  }
+    return res;
+}
+  Console.Write("Введите число A ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B ");
+  int B = Convert.ToInt32(Console.ReadLine());
+  int power = Exponentiation(A, B);
+  Console.WriteLine(power);
